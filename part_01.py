7eb945e34c9b384cea0f98fa27105309e7824320
Unknown <christopher.morrison@vanderbilt.edu>
2018-01-29 17:16:40 -0600

    This group of files consists of the code that I wrote for Assignment 1.

---
+++ part_01.py
@@ -0,0 +1,7 @@
+# Import the datadotworld module as dw
+import datadotworld as dw
+
+# Import the city council votes dataset
+dataset = dw.load_dataset(‘stephen-hoover/chicago-city-council-votes')
+
+
